632e19198a70c7a9da21547a89d5c384b582b28f
Guilherme <guiheurich@gmail.com>
2019-07-05 13:34:14 +0100

    Implement Cypress configuration
    Alternative ci implementation due to difficulties with nightwatch.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 var Noun = require('./src/noun.js')
 var random_noun = new Noun();
 
--- cypress/integration/nounSpec.js
+++ cypress/integration/nounSpec.js
@@ -1,8 +1,8 @@
-var Noun = require('../src/noun')
+var Noun = require('../../src/noun')
 
 describe('Noun', function(){
   it('generates a random noun', function(){
     var noun = new Noun();
-    expect(noun.generate()).toEqual('random noun')
+    expect(noun.generate()).to.eq('random noun')
   })
 });
