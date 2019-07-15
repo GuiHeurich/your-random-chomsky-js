9f8f0be5c000ff0bd2e543119fb40cfc7ce6b884
Guilherme <guiheurich@gmail.com>
2019-07-15 16:35:51 +0100

    Display random adjective
    App now displays random adjective alongside noun by handling one promise after the other.

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8080
 var Noun = require('./src/noun.js')
+var Adjective = require('./src/adjective.js')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 app.get('/', function (req, res) {
   var random_noun = new Noun();
-  random_noun.generate().then(function(results){
-    res.render('index', { sentence: results })
+  var random_adjective = new Adjective();
+  random_noun.generate().then(function(noun){
+    var sentence = []
+    sentence.push(noun)
+    random_adjective.generate().then(function(adjective){
+        sentence.push(adjective)
+        res.render('index', { sentence: sentence })
+      })
   })
 })
 
